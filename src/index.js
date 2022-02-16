643d0dd06e84b5c0f4b163285e4ea82dc66f8ca9
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-02-17 00:39:41 +0100

    navBar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+import green from "@material-ui/core/colors/green";
 
 import App from "./App";
 
+let theme = createTheme({
+  palette: {
+    primary: {
+      light: green[200],
+      main: green[400],
+      dark: green[800],
+    },
+  },
+});
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
