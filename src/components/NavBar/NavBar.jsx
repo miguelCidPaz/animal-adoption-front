3363dc4012ea222a2fad70ffd0be0c3bf0f87064
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-02-16 22:01:09 +0100

    initial process

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div className="nav">
+      <p>Esto es es Navbar</p>
+    </div>
+  );
+}
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import NavBar from "../components/NavBar/NavBar";
 
 function MainView() {
   return (
     <div>
+      <NavBar />
       <p>Estas en la home</p>
     </div>
   );
