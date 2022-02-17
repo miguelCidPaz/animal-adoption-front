1e86a6e0eb1bec767b248ee844dcf8344dd63501
fernandocieri <fernandocieri@gmail.com>
2022-02-17 18:24:16 +0100

    starts Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+    return (
+        <footer>
+        <section className="contact">
+            <ul>
+                <li>tel. 666 66 66 66</li>
+                <li>email: adoption@thebridge.com</li>
+                <li>schedule: </li>
+            </ul>
+        </section>
+        <section className="aboutUsLink"></section>
+        <section className="maps">
+           
+        </section>
+    </footer>
+    )
+    
+}
