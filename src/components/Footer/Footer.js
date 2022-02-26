6e3f787d9a13dae104654b7d5eaf399f110c2d2f
fernandocieri <fernandocieri@gmail.com>
2022-02-26 02:09:33 +0100

    Adds styles and finishes a first version of the component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,27 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import MapContainer from "./MapContainer";
 
 export default function Footer() {
     const mapSrc = "https://maps.google.com/maps?q=P.%C2%BA%20de%20Recoletos,%2015,%2028004%20Madrid&t=&z=13&ie=UTF8&iwloc=&output=embed";
 
     return (
         <footer>
-            <section className="contact">
+            <section className="footer-contact">
+                <div className="footer-title">Contact us</div>
                 <ul>
-                    <li>tel. 666 66 66 66</li>
+                    <li>tel: 666 66 66 66</li>
                     <li>email: adoption@thebridge.com</li>
-                    <li>schedule: </li>
+                    <li>opening hours: weekdays from 9:00 to 14:00</li>
                 </ul>
             </section>
-            <section className="aboutUsLink">
-                <Link to={'/about-us'}>
+            <section className="footer-aboutUs">
+                <div className="footer-title">About us</div>
+                <Link to={'/about-us'} className="routerLink">
                     learn more about our shelter
                 </Link>
             </section>
-            <section className="maps">
+            <section className="footer-maps">
+                <div className="footer-title">Find us</div>
                 <div className="gmap_canvas">
                     <iframe title='gmap'
                         id="gmap_canvas"
