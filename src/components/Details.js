62ff1cedc3b5864d9b6c44184d3e54e90ec9b4de
fernandocieri <fernandocieri@gmail.com>
2022-02-26 02:54:52 +0100

    Sets different sections of the component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,18 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
+import Button from '@mui/material/Button';
 
 export default function Details() {
+    const { id } = useParams();
+    const { pets, setPets } = useContext(petsDataContext);
     return (
-        <p>hello world</p>
+        <section>
+            {/* image component */}
+            <div className="details-petName"></div>
+            <div className="details-petDescription"></div>
+            <div className="details-petCategory"></div>
+            <Button variant="contained">Adopt</Button>
+        </section>
     )
 }
