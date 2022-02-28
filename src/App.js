69f63d9d9d0d6e7cfb246cd6924f47a33eb02a39
fernandocieri <fernandocieri@gmail.com>
2022-02-28 22:47:58 +0100

    Implements swiper library and adds most css styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./assets/styles.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
+import Details from "./components/Details";
 function App() {
   return (
     <main>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Details />} />
         </Routes>
       </BrowserRouter>
     </main>
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,32 +1,84 @@
 import * as React from "react";
 import { Link, useParams } from "react-router-dom";
 
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 export default function Details() {
     const { id } = useParams();
     //const { pets, setPets } = useContext(petsDataContext);
+    //mocked info
+    const pet = {
+        name: 'Galo',
+        description: 'Richi is still very young. He is a confident, energetic and good-tempered dog. Sociable with people and like-minded dogs, he needs a welcoming home to express his full potential.',
+        category: ['dog'],
+        images: ['https://adopcionanimal.es/wp-content/uploads/2022/02/Galo_4.jpg', 'https://adopcionanimal.es/wp-content/uploads/2022/02/Galo_1.jpg'],
+        gender: 'male',
+        breed: 'mixed',
+        size: 'big',
+        weight: 45,
+        rescued: '12/12/24'
+    }
     return (
-        <section>
-            {/* image component */}
-            <div class="imageBox">
+        <div className="component">
+            <section className="details-main">
+                <Swiper id="main"
+                    modules={[Navigation, Pagination, Scrollbar, A11y]}
+                    spaceBetween={1}
+                    slidesPerView={1}
+                    navigation
+                    pagination={{ clickable: true }}
+                    scrollbar={{ draggable: true }}>
+                    {
+                        pet.images.map((image) => (
+                            <SwiperSlide className="swiper-slide">
+                                <img src={`${image}`} />
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
+                {/* image component */}
+                {/* <div class="imageBox">
                 <div class="imageInn">
                     <img src="https://adopcionanimal.es/wp-content/uploads/2022/02/Galo_1.jpg" alt="Default Image"/>
                 </div>
                 <div class="hoverImg">
                     <img src="https://adopcionanimal.es/wp-content/uploads/2022/02/Galo_2.jpg" alt="Profile Image"/>
                 </div>
-            </div>
-            <section className="details__pet-data">
-                <div className="details__pet-name"></div>
-                <div className="details__pet-description"></div>
-                <div className="details__pet-category"></div>
-                <button className="details__adopt-button">
-                    <span>Adopt</span>
-                    <svg width="34" height="34" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="37" cy="37" r="35.5" stroke="white" stroke-width="4"></circle>
-                        <path d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z" fill="white"></path>
-                    </svg>
-                </button>
+            </div> */}
+                <section className="details__pet-data">
+                    <div className="details__pet-name">{pet.name}</div>
+                    <div className="details__pet-description">{pet.description}</div>
+                    <div className="details__pet-category">
+                        <span className="category">Category:</span> {pet.category.map((category) => <span className="category-name">{category}</span>)}
+                    </div>
+                    <button className="details__adopt-button">
+                        <span>Adopt</span>
+                        <svg width="34" height="34" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="37" cy="37" r="35.5" stroke="white" stroke-width="4"></circle>
+                            <path d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z" fill="white"></path>
+                        </svg>
+                    </button>
+                </section>
             </section>
-        </section>
+            <section className="details-description">
+                <div className="description-title">Description</div>
+                <ul>
+                    <li>Gender: {pet.gender}</li>
+                    <li>Breed: {pet.breed}</li>
+                    <li>Size: {pet.size}</li>
+                    <li>Weight: {pet.weight}kg</li>
+                    <li>Recued: {pet.rescued}</li>
+                </ul>
+            </section>
+        </div>
+
     )
 }
