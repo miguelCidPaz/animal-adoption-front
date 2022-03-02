6053c668d4e7698bed02b3b022e134552f671bb1
fernandocieri <fernandocieri@gmail.com>
2022-03-02 22:00:53 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/details/:id" element={<Details />} />
+          <Route path="pet/:id/details" element={<Details />} />
         </Routes>
       </BrowserRouter>
     </main>
