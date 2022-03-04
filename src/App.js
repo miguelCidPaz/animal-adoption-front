b5933315923e5d6852be5278b9bdeb6f34b0fe78
fernandocieri <fernandocieri@gmail.com>
2022-03-04 20:47:51 +0100

    Refactors folders

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./assets/styles.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
-import Details from "./components/Details";
+import Details from "./views/Details";
 function App() {
   return (
     <main>
--- src/views/Details.js
+++ src/views/Details.js
@@ -8,7 +8,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-import useFindCurrentPet from "./hooks/useCurrentPet";
+import useFindCurrentPet from "../hooks/useCurrentPet";
 
 export default function Details() {
     const { id } = useParams();
