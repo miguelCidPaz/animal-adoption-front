f66525c38c0e33868293d912d388aa9e7566ff21
josemsb92 <josemsb92@gmail.com>
2022-03-04 21:12:30 +0100

    stash

---
+++ src/components/PetCard.js
@@ -0,0 +1,18 @@
+export default function PetCard() {
+  return (
+    <div id="imageBox">
+      <div className="imageInn">
+        <img
+          src="https://adopcionanimal.es/wp-content/uploads/2022/02/Galo_1.jpg"
+          alt="Default Image"
+        />
+      </div>
+      <div class="hoverImg">
+        <img
+          src="https://adopcionanimal.es/wp-content/uploads/2022/02/Galo_2.jpg"
+          alt="Profile Image"
+        />
+      </div>
+    </div>
+  );
+}
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Pet from '../components/PetCard'
 function MainView() {
   return (
     <div>
       <p>Estas en la home</p>
+      <Pet/>
     </div>
   );
 }
