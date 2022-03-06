d62750fab8e0ec072ec1fe830e74714df8a2d2ac
josemsb92 <josemsb92@gmail.com>
2022-03-06 22:44:11 +0100

    classNames redefined and routing Link on PetCard almos defined.

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,25 +1,26 @@
-export default function PetCard() {
+import { Link } from "react-router-dom";
+export default function PetCard(props) {
   return (
-    <div className="cardBox" >
-      <div id="imageBox"> 
+    <div className="petcard-box" >
+      <div id="petcard-img_box"> 
       <img
-        className="bottomimg"
+        class="petcard-img_bottom"
         src="https://adopcionanimal.es/wp-content/uploads/2022/02/Galo_1.jpg"
         alt="Default Image"
       />
 
       <img
-        class="topimg"
+        class="petcard-img_top"
         src="https://adopcionanimal.es/wp-content/uploads/2022/02/Galo_2.jpg"
         alt="Profile Image"
       />
       </div>      
 
-      <div className="options">
+      <div className="petcard-options">
         <p>Richi</p>
-        <div className="buttonsFlex">
-        <button>adopt</button>
-        <button>details</button>
+        <div className="petcard-options_buttons">
+        <Link to=""><button>adopt</button></Link>
+        <Link to={`/api/pets/${props.id}`}><button>details</button></Link>
         </div>        
       </div>
     </div>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import Pet from '../components/PetCard'
 function MainView() {
   return (
-    <div>
-      <p>Estas en la home</p>   
+    <div >
+      <p>Estas en la home</p>  
+      <div className="home">
+      <Pet id="a"/> 
+      <Pet/>
+      <Pet/>
+      <Pet/>
+      <Pet/>
+      <Pet/>
+      <Pet/>
+      <Pet/>
+      <Pet/>
+      </div>
+      
     </div>
   );
 }
